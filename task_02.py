bc7f420ca500e74d3f4bac0e406a90f3c645b749
SergoBushmakin <Sergo.Bushmakin76@spsmail.cuny.edu>
2015-10-04 23:51:46 +0000

    completed warmup5

---
+++ task_02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 2."""
+
+
+from task_01 import peanut
+
+TIME = peanut.BUTTER
---
+++ task_03.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 3."""
+
+
+from task_01 import peanut
+
+BUTTER = peanut.BUTTER
+JELLY = BUTTER
